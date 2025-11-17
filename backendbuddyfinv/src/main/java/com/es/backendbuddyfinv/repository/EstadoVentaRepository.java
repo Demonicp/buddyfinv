bce98e617e575a5de207f6ca63a1f9ff79549cfd
moreno_esteban <esteban.roa@correounivalle.edu.co>
2025-11-17 11:02:24 -0500

    implementacion de repositorios y de Dto's

---
+++ backendbuddyfinv/src/main/java/com/es/backendbuddyfinv/repository/EstadoVentaRepository.java
@@ -0,0 +1,11 @@
+package com.es.backendbuddyfinv.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.es.backendbuddyfinv.model.EstadoVenta;
+
+@Repository
+public interface EstadoVentaRepository extends JpaRepository<EstadoVenta, Long> {
+    // métodos custom si los necesitas
+}
--- backendbuddyfinv/src/main/java/com/es/backendbuddyfinv/repository/ProductoRepository.java
+++ backendbuddyfinv/src/main/java/com/es/backendbuddyfinv/repository/ProductoRepository.java
@@ -1,13 +1,14 @@
 package com.es.backendbuddyfinv.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.es.backendbuddyfinv.model.Producto;
-import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface ProductoRepository extends JpaRepository<Producto, Long> {
@@ -19,15 +20,29 @@ public interface ProductoRepository extends JpaRepository<Producto, Long> {
     JOIN FETCH p.propietario
     JOIN FETCH p.detalleInventarios d
     WHERE p.propietario.id = :idPropietario
-""")
-List<Producto> findByPropietarioConInventario(@Param("idPropietario") Long idPropietario);
-/////SANTIAGO MODIFCIAR PRODUCTO
-List<Producto> findByPropietarioId(Long propietarioId);
-List<Producto> findByPropietarioUsuario(String usuario);
+    """)
+    List<Producto> findByPropietarioConInventario(@Param("idPropietario") Long idPropietario);
+    /////SANTIAGO MODIFCIAR PRODUCTO
+    /// en crear ventas esteban moreno tambine hace uso de este metodo
+    List<Producto> findByPropietarioId(Long propietarioId);
+    List<Producto> findByPropietarioUsuario(String usuario);
 
-// Caso 1: El producto pertenece directamente al propietario
-@Query("SELECT p FROM Producto p WHERE p.idProducto = :idProducto AND p.propietario.id = :idPropietario")
-Optional<Producto> findByIdAndPropietario(@Param("idProducto") Long idProducto, @Param("idPropietario") Long idPropietario);
-/////FIN SANTIAGO MODDIFICAR PRODUCTO
-    
+    // Caso 1: El producto pertenece directamente al propietario
+    @Query("SELECT p FROM Producto p WHERE p.idProducto = :idProducto AND p.propietario.id = :idPropietario")
+    Optional<Producto> findByIdAndPropietario(@Param("idProducto") Long idProducto, @Param("idPropietario") Long idPropietario);
+    /////FIN SANTIAGO MODDIFICAR PRODUCTO
+
+        
+
+
+    //implementacion esteban moreno roa /feature/ crear venta
+
+    List<Producto> findByTipoProductoIdTipoProducto(Long idTipoProducto);
+
+    List<Producto> findByEstadoProductoIdEstadoPro(Long idEstadoPro);
+
+    Optional<Producto> findByIdProductoAndPropietarioId(Long idProducto, Long propietarioId);
+
+
+
 }
